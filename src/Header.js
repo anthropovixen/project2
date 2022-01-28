bdd975b79eb5b803cd4a913c4f361112f288775a
Tanimara Santos <36615712+anthropovixen@users.noreply.github.com>
2022-01-28 13:56:47 +1100

    Create Header.js
    Create structure for header

---
+++ src/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+import reactDom from "react-dom";
+import headshot from "./images/headshot-square-220kb.png";
+
+export default function Header() {
+  return <img src={headshot} alt="Tanimara Elias Santos headshot" />;
+}
