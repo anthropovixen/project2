de9bd080666841b7154cbb5fae53f60bcf7f24fe
Tanimara Santos <36615712+anthropovixen@users.noreply.github.com>
2022-02-02 13:03:36 +1100

    Create Footer.js
    change folder for Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+  FaTwitter,
+  FaFacebookSquare,
+  FaInstagramSquare,
+  FaLinkedin,
+  FaGithub,
+} from "react-icons/fa";
+
+export default function Footer() {
+  return (
+    <div className="footer--icons">
+      <FaTwitter />
+      <FaFacebookSquare />
+      <FaInstagramSquare />
+      <FaLinkedin />
+      <FaGithub />
+    </div>
+  );
+}
