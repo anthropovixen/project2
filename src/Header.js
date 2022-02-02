58512011909275fd1139ddd962894dff870c2570
Tanimara Santos <36615712+anthropovixen@users.noreply.github.com>
2022-02-02 13:04:34 +1100

    Create folder components
    change folder for components

--- src/Header.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import reactDom from "react-dom";
-import headshot from "./images/headshot-square-220kb.png";
-
-export default function Header() {
-  return <img src={headshot} alt="Tanimara Elias Santos headshot" />;
-}
